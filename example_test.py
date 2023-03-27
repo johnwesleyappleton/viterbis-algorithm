8d7fef9c39639800819167f6db581bf24518133f
John-Wesley Appleton <johnwesleyappleton@gmail.com>
2023-03-27 18:38:32 -0400

    Update example_test.py

--- example_test.py
+++ example_test.py
@@ -1,34 +1,5 @@
 import numpy as np
-from histogram_filter import HistogramFilter
 from HMM_solution import HMM
-
-'''
-Test for Bayes filter
-'''
-
-# load the data
-data = np.load(open('starter.npz', 'rb'))
-cmap = data['arr_0']
-actions = data['arr_1']
-observations = data['arr_2']
-belief_states = data['arr_3']
-
-# test your code here
-bayes_filter = HistogramFilter()
-
-n, m = cmap.shape
-belief = np.full((n, m), 1/(n*m))
-posterior = np.zeros((len(actions), 2), dtype=int)
-
-for i in range(len(actions)):
-    belief = bayes_filter.histogram_filter(cmap, belief, actions[i], observations[i])
-    belief = np.flip(belief, 0)
-    argmax = np.argmax(belief)
-    posterior[i, 0] = argmax % m
-    posterior[i, 1] = argmax // m
-    belief = np.flip(belief, 0)
-
-print("bayes filter correct implementation: %s" % np.array_equal(belief_states[-1], posterior[-1]), "\n")
 
 
 '''
